b0f83d8f352d9d5518da3643816b17d34bd58a56
lupsups <lupemaydana@gmail.com>
2017-05-13 01:18:33 -0400

    Puertas para pasar de nivel y fade in , fade out
    Ya se ponen las puertas y los collider para que cuando lleges alli pases
    al siguiente nivel

---
+++ Project/Assets/Scripts/PasarNivel.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PasarNivel : MonoBehaviour {
+	ScreenFader screenFader;
+	public float tiempo=10;
+	public Transform posicionInicioNivel;
+	Transform jugador;
+	void Awake () {
+		screenFader = GetComponent<ScreenFader> ();
+	}
+		
+	void OnTriggerEnter(Collider otro){
+		//gameover()
+		//ir a pantalla de mperdiste
+		//SceneManager.LoadScene ("Nivel2", LoadSceneMode.Single);
+		//Application.LoadLevel("Ganaste");
+		if(otro.CompareTag ("Player")){
+			jugador = otro.transform;
+			StartCoroutine (loadScene (screenFader.fadeTime));
+		}
+	}
+	public IEnumerator loadScene(float time){
+		{
+			screenFader.fadeIn = false;
+			yield return new WaitForSeconds (tiempo);
+			jugador.Translate (posicionInicioNivel.position);
+			screenFader.fadeIn = true;
+		}
+	}
+}
