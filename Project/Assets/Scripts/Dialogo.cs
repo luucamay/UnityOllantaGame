3ba83984908ea14f2f91659f1111c128315dee5b
lupsups <lupemaydana@gmail.com>
2017-05-16 02:23:08 -0400

    Se ha añadido las preguntas para el personaje sabío del juego
    se puede interactuar en las respuesta, te pinta de color verde si una
    respuesta es correcta, o de color rojo si la respuesta es incorrecta

---
+++ Project/Assets/Scripts/Dialogo.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Dialogo : MonoBehaviour
+{
+	public string[] textos;
+	public float tiempoDuracionTexto = 2f;
+	Text texto;
+	public float timer;
+	int numeroTextos;
+	int indice;
+	GameObject jugador;
+	public Canvas canvas;
+	void Start ()
+	{
+		timer = tiempoDuracionTexto;
+		indice = 0;
+		numeroTextos = textos.Length;
+		texto = GetComponentInChildren <Text> ();
+		//para rotar a la sabia anciana hacia el jugador
+		jugador = GameObject.FindGameObjectWithTag ("Player");
+		transform.rotation = Quaternion.LookRotation (jugador.transform.forward*-1);
+
+	}
+
+	void Update ()
+	{
+		timer += Time.deltaTime;
+		if (timer > tiempoDuracionTexto) {
+			timer = 0;
+			if (indice < numeroTextos) {
+				texto.text = textos [indice];
+				indice++;
+			} else {
+				texto.text = "";
+				//activa el canvas que tiene los botones de preguntas
+				canvas.gameObject.SetActive (true);
+				//la idea es que no avance cuando aprete los botones
+				//jugador.GetComponent <VRWalkWhilePressing>().enabled=false;
+			}
+		}
+	}
+}
